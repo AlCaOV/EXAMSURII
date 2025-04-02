2e691f4a94bdba40eefdda6167906393e40ab349
AlCaOV <yurekserednij@gmail.com>
2025-04-03 00:44:00 +0300

    final

--- demo/src/main/java/com/example/demo/Controller/disciplineC.java
+++ demo/src/main/java/com/example/demo/Controller/disciplineC.java
@@ -4,6 +4,8 @@ package com.example.demo.Controller;
 import com.example.demo.Model.discipline;
 import com.example.demo.Repository.DisciplineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -84,11 +86,14 @@ public class disciplineC {
     }
 
     // Видалення продукту за ID
-    @DeleteMapping("/{id}")
-    public void deletediscipline(@PathVariable Integer id) {
-        discipline discipline = new discipline();
-        discipline.deletediscipline(id);
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity deletediscipline(@PathVariable Integer id) {
+        try {
+            discipline discipline = new discipline();
+           discipline.deletediscipline(id);
+            return ResponseEntity.ok("Product deleted successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to delete discipline");
+        }
     }
-
-
 }
